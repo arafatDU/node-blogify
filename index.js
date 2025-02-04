7a922e1d85b74f37d4b005afe3c545d21de29c4c
arafatDU <bsse1435@iit.du.ac.bd>
2025-02-04 14:58:26 +0600

    Authentication completed

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
+const userRoute = require("./routes/user");
+const { checkForAuthenticationCookie } = require("./middlewares/authentication");
+
 
 const app = express();
 const PORT = process.env.PORT || 8000;
@@ -19,11 +22,16 @@ app.set("views", path.resolve("./views"));
 
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
 
 
 app.get("/", (req, res) => {
-  res.render("home");
+  res.render("home", {
+    user: req.user,
+  });
 });
+
+app.use("/user", userRoute);
 
 
 app.listen(PORT, () => {
